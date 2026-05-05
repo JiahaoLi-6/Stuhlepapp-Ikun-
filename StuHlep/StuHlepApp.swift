a823a23ad0ad8c2a9d3c83e53066124e4586aec1
JiahaoLi-6 <Jiahao.Li-6@student.uts.edu.au>
2026-05-05 16:18:07 +1000

    Initial Commit

---
+++ StuHlep/StuHlepApp.swift
@@ -0,0 +1,17 @@
+//
+//  StuHlepApp.swift
+//  StuHlep
+//
+//  Created by 1 on 5/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct StuHlepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
